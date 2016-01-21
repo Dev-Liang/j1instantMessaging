2b83215d9052bcf83af21e87d10e12c4fef521c7
liangjian <liangjian1@j1.com>
2016-01-21 16:26:44 +0800

    初始化了项目

---
+++ j1instantMessaging/j1instantMessaging/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  j1instantMessaging
+//
+//  Created by liang on 16/1/21.
+//  Copyright © 2016年 liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
